d9414d3b1d79e1d8654974071c74f26c93de515b
Glitch (hello-express) <none>
2020-09-26 15:01:52 +0000

    🗾🎫 Checkpoint ./commands/ideia.js:22584665/247 ./what.json:22584665/2 ./.glitch-assets:22584665/764

--- commands/ideia.js
+++ commands/ideia.js
@@ -9,7 +9,7 @@ if (!args[0]) {
 } else if (content.length > 1000) {
   return message.channel.send(`${message.author.username}, forneça uma sugestão de no máximo 1000 caracteres.`);
 } else {
-  var canal = message.guild.channels.cache.find(ch => ch.id === "699780010235527278");
+  var canal = message.guild.channels.cache.find(ch => ch.id === "660578281048768512");
   const msg = await canal.send(
     new Discord.MessageEmbed()
     .setColor("#FFFFF1")
@@ -20,7 +20,7 @@ if (!args[0]) {
   );
   await message.channel.send(`${message.author} a mensagem foi enviada com sucesso!`);
 
-  const emojis = ["✔️", "❎"];
+  const emojis = ["https://cdn.glitch.com/c9774cdd-15e1-4938-8fde-4aeedbb8b529%2F5845_tickgreen.gif?v=1601132470229", ""];
 
   for (const i in emojis) {
     await msg.react(emojis[i])
