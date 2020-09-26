270ff3983932bd3dd445ed4b71c1502a451b53bc
Glitch (hello-express) <none>
2020-09-26 14:50:35 +0000

    🐄👫 Checkpoint ./commands/ideia.js:22584665/921 ./what.json:22584665/4 ./commands/kiss.js:22584665/37 ./commands/ping.js:22584665/576

--- commands/kiss.js
+++ commands/kiss.js
@@ -24,7 +24,7 @@ let avatar = message.author.displayAvatarURL({format: 'png'});
         .setImage(rand)
         .setTimestamp()
         .setThumbnail(avatar)
-        .setFooter('Kissu kissu kissu')
+        .setFooter('Beijos Beijos Beijos')
         .setAuthor(message.author.tag, avatar);
   await message.channel.send(embed);
 }
