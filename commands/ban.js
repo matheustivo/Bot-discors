60c472cf6f3d5f2d2542b6fbc3cd814ae657407f
Glitch (hello-express) <none>
2020-09-26 19:11:46 +0000

    📖💂 Checkpoint ./commands/ban.js:22584665/2

--- commands/ban.js
+++ commands/ban.js
@@ -3,11 +3,11 @@ const Discord = require("discord.js");
 exports.run =  (client, message, args) => {
 
     var membro = message.mentions.members.first() || message.guild.member.cache.get(args[0]);
-    if (!membro) return message.reply(`mencione um usuario! ex.: !ban @membro [motivo]`)
+    if (!membro) return message.reply(`mencione um usuario! ex.: x!ban @membro [motivo]`)
     if (membro === message.member) return message.reply(`vc nao pode banir vc mesmo.`)
 
     var motivo = args.slice(1).join(" ");
-    if (!motivo) return message.reply(`escreva o motivo! ex.: !ban @membro [motivo]`)
+    if (!motivo) return message.reply(`escreva o motivo! ex.: x!ban @membro [motivo]`)
     if (!message.member.hasPermission("BAN_MEMBERS")) return message.reply(`vc precisa da permissao **Banir Membros**.`)
 
     var canal = bot.channels.get("660569504815775755");
